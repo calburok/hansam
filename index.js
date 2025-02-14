6ada056a26a35ad1a43a6676f67c7f3832251cd5
LHB <dlgusqo1129@gmail.com>
2025-02-14 17:55:47 +0900

    "lhb 한샘4차"

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 
@@ -75,6 +76,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 TL.fromTo(
@@ -84,6 +86,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 TL.fromTo(
@@ -93,6 +96,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 TL.fromTo(
@@ -102,6 +106,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 
@@ -112,6 +117,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 
@@ -122,6 +128,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 
@@ -132,6 +139,7 @@ TL.fromTo(
   },
   {
     opacity: 1,
+    duration: 0.25,
   }
 );
 
@@ -146,8 +154,9 @@ const TL1 = gsap.timeline({
 });
 
 TL1.to(".star1", { rotate: "720deg", duration: 30 }, 0);
-
+TL1.to(".star2", { rotate: "-720deg", duration: 30 }, 0);
 TL1.to(".star3", { rotate: "-720deg", duration: 30 }, 0);
+
 TL1.to(".star4", { rotate: "-720deg", duration: 30 }, 0);
 
 TL1.to(".star6", { rotate: "720deg", duration: 30 }, 0);
@@ -193,4 +202,209 @@ TL2.to(".star5", {
   duration: 2,
   repeat: -1,
   yoyo: true,
+});
+
+const TL3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".service",
+    start: "top top",
+    end: "150% top",
+    pin: true,
+    scrub: true,
+    markers: true,
+  },
+});
+
+TL3.to(".service__wrap", {
+  duration: 2.5,
+  ease: "expoScale(0.5,7,none)",
+  x: -800,
+});
+
+const TL4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".spring",
+    start: "30% bottom",
+    end: "10% top",
+    scrub: 1,
+    markers: true,
+  },
+});
+
+TL4.to(".spring__bigcard", {
+  scale: 1,
+  duration: 1,
+  yoyo: true,
+});
+
+// 호버 애니메이션
+
+const peoplebox = document.querySelector(".peoplebox");
+const peopleline = document.querySelector(".peopleline");
+const peopleline2 = document.querySelector(".peopleline2");
+const cardTop = document.querySelector(".cardTop ");
+const person = document.querySelector(".person");
+const person1 = document.querySelector(".person1");
+
+const TL5 = gsap.timeline({
+  scrub: true,
+});
+
+cardTop.addEventListener("mouseover", () => {
+  TL5.to(".person", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person1", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person2", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person3", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person4", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person5", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person6", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person7", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person8", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person9", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person10", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person11", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person12", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person13", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person14", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person15", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person16", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person17", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person18", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person19", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person20", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person21", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person22", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person23", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person24", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person25", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person26", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person27", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person28", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person29", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person30", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person31", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person32", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person33", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person34", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person35", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person36", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person37", {
+    opacity: 1,
+    duration: 0.25,
+  });
+  TL5.to(".person38", {
+    opacity: 1,
+    duration: 0.25,
+  });
 });
